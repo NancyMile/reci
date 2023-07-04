25e88a0b6421ad742241ea3118820972f568e075
nancy <smithnancy@outlook.com>
2023-07-04 22:52:38 +1000

    component recipe is  displaying the drinks  correctly, image description and a button to access the recipe

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -7,8 +7,22 @@
 </script>
 
 <template>
-    <div>
-        <h2>{{ recipe.strDrink }}</h2>
+    <div class="border shadow">
+        <div class="overflow-hidden">
+            <img
+                :src="recipe.strDrinkThumb"
+                alt="'image '+ recipe.strDrink"
+                class="hover:scale-125 transition-transform hover:rotate-2"
+            >
+        </div>
+        <div class="p-5">
+            <h2 class="text-2xl truncate font-extrabold">{{ recipe.strDrink }}</h2>
+            <button
+                class="bg-orange-400  hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+            >
+                Recipe
+            </button>
+        </div>
     </div>
 
 </template>
