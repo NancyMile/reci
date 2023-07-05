623d2efe99f47bf93f0e597a0ff1825869e7a1bc
nancy <smithnancy@outlook.com>
2023-07-05 16:21:42 +1000

    highlight  the nav option of the ctual page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,17 +32,17 @@
                         <img src="/img/logo.svg" class="w-32" alt="logo">
                     </RouterLink>
                 </div>
-                <nav class="flex gap-4">
+                <nav class="flex gap-4 text-white">
                     <RouterLink
                     :to="{name: 'home'}"
-                    class="text-white uppercase font-bold"
+                    class="uppercase font-bold"
                     active-class="text-orange-500"
                     >
                         Home
                     </RouterLink>
                     <RouterLink
                     :to="{name: 'favorites'}"
-                    class="text-white uppercase font-bold"
+                    class="uppercase font-bold"
                     active-class="text-orange-500">
                         Favorites
                     </RouterLink>
