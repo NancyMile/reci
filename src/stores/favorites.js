29fd4bf06f7bafb53b2cf2d369176a9456457243
nancy <smithnancy@outlook.com>
2023-07-05 16:05:40 +1000

    getting favorites and saving them on localstorage

--- src/stores/favorites.js
+++ src/stores/favorites.js
@@ -1,14 +1,31 @@
 import { defineStore } from "pinia";
+import { useDrinksStore } from "./drinks";
+import { ref, watch, onMounted } from "vue";
 
 export const useFavoritesStore = defineStore('favorites', () => { 
 
+    const drinks = useDrinksStore()
+
+    const favorites = ref([])
+
+    watch(favorites, () => {
+        synchronizeLocalStorage()
+    }, {
+        deep: true
+    })
+
+    const synchronizeLocalStorage = () => {
+        localStorage.setItem('favorites',JSON.stringify(favorites.value))
+     }
 
     const handleFavorites = () => { 
-        console.log('adding ....')
+        //console.log('adding ....'+drinks.recipe.strDrink)
+        favorites.value.push(drinks.recipe)
     }
 
     return {
-        handleFavorites
+        handleFavorites,
+        favorites
 
     }
 })
